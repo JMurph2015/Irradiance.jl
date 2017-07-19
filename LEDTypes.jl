e91afccca62f837906bdca31d26e284286f3fe67
Joseph Murphy <air.jmurph@gmail.com>
2017-07-19 00:03:05 -0400

    Did some more work on the datatypes, I think we're getting close there.

--- LEDTypes.jl
+++ LEDTypes.jl
@@ -1,21 +1,18 @@
 using Colors, Interpolations
 import Base.getindex, Base.length, Base.setindex!, Base.push!, Base.setindex
+
+abstract type AbstractChannel end
+abstract type AbstractController end
+
+
 # Make some types so that I don't go insane
-mutable struct LEDArray
-    controllers::Array{LEDController}
-    channels::Array{LEDChannel}
-    strips::Array{LEDStrip}
-end
-
-
-
-mutable struct LEDStrip
+mutable struct LEDStrip{T,N}
     name::String
-    channel::LEDChannel
-    controller::LEDController
+    channel::T
+    controller::N
     startAddr::Int
     endAddr::Int
-    function LEDStrip(name::String, channel::LEDChannel, controller::LEDController, startAddr::Int, endAddr::Int)
+    function LEDStrip{T,N}(name::String, channel::T, controller::N, startAddr::Int, endAddr::Int) where {T<:AbstractChannel, N<:AbstractController}
         x = new(name, channel, controller, startAddr, endAddr)
         push!(channel, x)
         push!(controller, x)
@@ -24,11 +21,19 @@ mutable struct LEDStrip
 end
 
 length(strip::LEDStrip) = length(strip.endAddr - strip.startAddr + 1)
-getindex(strip::LEDStrip, i::Any) = getindex(strip.controller.addrs, i + strip.startAddr)
+getindex(strip::LEDStrip, i::Any) = getindex(strip.controller.addrs, i + strip.startAddr-1)
+function setindex!{T<:AbstractChannel, N<:AbstractController}(strip::LEDStrip{T, N}, val::Color, idx::Int)
+    strip.controller[idx+strip.startAddr-1] = val
+end
+function setindex{T<:AbstractChannel, N<:AbstractController}(strip::LEDStrip{T, N}, val::Color, idx::Int)
+    new_strip = deepcopy(strip)
+    new_strip.controller[idx+strip.startAddr-1] = val
+    return new_strip
+end
 
 
 
-mutable struct LEDController
+mutable struct LEDController <: AbstractController
     addrs::Array{ColorTypes.RGB{FixedPointNumbers.Normed{UInt8,8}},1}
     strips::Array{LEDStrip}
     location::Tuple{IPAddr, Int}
@@ -36,23 +41,33 @@ mutable struct LEDController
         return new(Array{ColorTypes.RGB{FixedPointNumbers.Normed{UInt8,8}},1}(sz), Array{LEDStrip, 1}(0), location)
     end
 end
+
 length(controller::LEDController) = length(controller.addrs)
 push!(controller::LEDController, val::LEDStrip) = push!(controller.strips, val)
+function setindex!(controller::LEDController, val::Color, idx::Int)
+    controller.addrs[idx] = val
+end
+function setindex(controller::LEDController, val::Color, idx::Int)
+    new_controller = deepcopy(controller)
+    new_controller.addrs[idx] = val
+    return new_controller
+end
 
 
 
-mutable struct LEDChannel
+mutable struct LEDChannel <: AbstractChannel
     strips::Array{LEDStrip}
     LEDChannel() = new(Array{LEDStrip, 1}(0))
 end
 
-length(channel::LEDChannel) = max(length.(channel.strips))
+getindex(channel::LEDChannel, idx::Int) = getindex(channel.strips[indmax(length.(channel.strips))], idx)
+length(channel::LEDChannel) = maximum(length.(channel.strips))
 push!(channel::LEDChannel, val::LEDStrip) = push!(channel.strips, val)
 
-function setindex!(channel::LEDChannel, i::Int, val::Color)
+function setindex!(channel::LEDChannel, val::Color, i::Int)
     i >= length(channel) || error("Index Out Bounds For This Channel")
 
-    homogeneous = True
+    homogeneous = true
     for i in 2:length(channel.strips)
         if length(channel.strips[i]) != length(channel.strips[i-1])
             homogeneous = false
@@ -60,7 +75,7 @@ function setindex!(channel::LEDChannel, i::Int, val::Color)
         end
     end
     if homogeneous
-        setindex!.(channel.strips, i, val)
+        setindex!.(channel.strips, val, i)
     else
         max_length = indmax(length.(channel.strips))
         channel.strips[max_length][i] = val
@@ -73,10 +88,10 @@ function setindex!(channel::LEDChannel, i::Int, val::Color)
     end
 end
 
-function setindex(channel::LEDChannel, i::Int, val::Color)
+function setindex(channel::LEDChannel, val::Color, i::Int)
     i >= length(channel) || error("Index Out Bounds For This Channel")
 
-    new_channel = copy(channel)
+    new_channel = deepcopy(channel)
     homogeneous = True
     for i in 2:length(new_channel.strips)
         if length(new_channel.strips[i]) != length(new_channel.strips[i-1])
@@ -96,4 +111,16 @@ function setindex(channel::LEDChannel, i::Int, val::Color)
             end
         end
     end
+    return new_channel
+end
+
+mutable struct LEDArray
+    controllers::Array{LEDController}
+    channels::Array{LEDChannel}
+    strips::Array{LEDStrip}
+end
+
+
+function LEDStrip(name::String, channel::LEDChannel, controller::LEDController, startAddr::Int, endAddr::Int)
+    return LEDStrip{LEDChannel, LEDController}(name, channel, controller, startAddr, endAddr)
 end
--- ParseConfig.jl
+++ ParseConfig.jl
@@ -1,13 +1,14 @@
 using JSON
 
 function parse_config(filename)
+    json_data = Dict()
     open(filename, "r") do f
         json_data = JSON.parse(f)
     end
-    "strips" in json_data || error("Config file was invalid")
-    "controllers" in json_data || error("Config file missing controller section")
+    "strips" in keys(json_data) || error("Config file was invalid")
+    "controllers" in keys(json_data) || error("Config file missing controller section")
     known_channels = Array{Int,1}(0)
-    known_controllers = Array{Int,1}(0)
+    known_controllers = Array{String,1}(0)
     for strip in json_data["strips"]
         if !(strip["channel"] in known_channels)
             push!(known_channels, strip["channel"])
@@ -22,12 +23,16 @@ function parse_config(filename)
     num_controllers = length(known_controllers)
     channel_to_idx = Dict(num => findfirst(known_channels,num) for num in known_channels)
     controller_to_idx = Dict(num => findfirst(known_controllers,num) for num in known_controllers)
-    channels = Array{LEDChannels, 1}(num_channels)
-    controllers = Array{LEDControllers, 1}(num_controllers)
+    channels = Array{LEDChannel, 1}(num_channels)
+    for i in eachindex(channels)
+        channels[i] = LEDChannel()
+    end
+    controllers = Array{LEDController, 1}(num_controllers)
     for i in eachindex(controllers)
         controller_json = json_data["controllers"][known_controllers[i]]
-        controllers[i].location = (IPAddr(controller_json["ip"]), controller_json["port"])
-    strips = Array{LEDControllers, 1}(0)
+        controllers[i] = LEDController(controller_json["size"], (IPv4(controller_json["ip"]), controller_json["port"]))
+    end
+    strips = Array{LEDStrip, 1}(0)
     for strip in json_data["strips"]
         tmp_channel = channels[channel_to_idx[strip["channel"]]]
         tmp_controller = controllers[controller_to_idx[strip["controller"]]]
